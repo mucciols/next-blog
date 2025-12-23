612ee2e87678bb6f11b9f433beb5be741677dba0
Simone Muccioli <simone.muccioli@consultant.aruba.it>
2025-12-23 17:01:12 +0100

    sasa

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,14 @@ function HomePage() {
     <>
       <Head>
         <title>My Blog</title>
-        <meta name="description" value="this is my blog" />
       </Head>
       <main>
         <h1>My Blog</h1>
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ul>
       </main>
     </>
   );
